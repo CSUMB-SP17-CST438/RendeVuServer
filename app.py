56102eded528cdc71c6f655a6c95c66b5d14f0b3
Salvador Hernandez <salvhernandez@csumb.edu>
2017-04-05 20:30:09 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import json
+import requests
+import time
+from flask import Flask, render_template, request, jsonify, abort, make_response
+
+app = Flask(__name__)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/api/v1.0/postInfo', methods=['POST'])
+def create_task():
+    print "someone pinged this"
+    print request.json
+    
+    #if the json data does not have the 'usedID' header
+    #it will return a 400
+    if not request.json or not 'userID' in request.json:
+        abort(400)
+    locationData = {
+        'userID': request.json['userID'],
+        'latitude': request.json['latitude'],
+        'longitude': request.json['longitude'],
+        'timestamp' : request.json['timestamp']
+    }
+    #tasks.append(task)
+    return jsonify({'data': locationData}), 200
+
+@app.route('/')
+def hello():
+    return "Hello world", 200
+
+if __name__ == '__main__':
+    app.run(
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080))
+    )
